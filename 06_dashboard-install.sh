3855c296e18df8e2b761ab3a7718e478807f12a8
shane <shane@digitalsanctum.com>
2020-06-22 12:39:55 -0700

    added dashboard install

---
+++ 06_dashboard-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+kubectl apply -f files/dashboard-admin-user.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml
+
+echo "Done!"
